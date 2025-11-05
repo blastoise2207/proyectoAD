79c3b820826d0dc76ab5bc664fa2185e5c04c62f
Brian AB <brianangulo900@gmail.com>
2025-11-05 00:19:01 -0400

    hola2

--- src/pagina/modeloC.jsx
+++ src/pagina/modeloC.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function MC() {
   return (
     <div>
-      <h1>Modelo de Comportamiento</h1>
+      <h1>Modelo Comportamiento</h1>
       <h2>Diagrama de Flujo de Datos</h2>
       <h2>Diagrama de Transicion de Estados</h2>
       <h2>Diagrama Entidad Relacion</h2>
